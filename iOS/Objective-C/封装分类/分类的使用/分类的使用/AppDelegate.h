ce0051271e6975f46941de4b1cffdf085421acc1
cwhzm <2451072587@qq.com>
2019-10-16 09:01:59 +0800

    no message

---
+++ iOS/Objective-C/封装分类/分类的使用/分类的使用/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  分类的使用
+//
+//  Created by czm on 2019/10/16.
+//  Copyright © 2019 czm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
